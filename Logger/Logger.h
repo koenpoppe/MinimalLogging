217b3c275761a0753c93ea9b7ed5e95c2c34526d
Koen Poppe <18189441+koenpoppe@users.noreply.github.com>
2023-12-17 13:22:28 +0100

    Meeting C++ 2023 Slide 11 - Record the point in time
    No formatting, no calendar calculations, just the minimal information to be able to restore the time when something was logged

--- Logger/Logger.h
+++ Logger/Logger.h
@@ -1,9 +1,24 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
+#include <chrono>
+
 template <int foo>
 class Logger
 {
+public:
+    using Clock = std::chrono::high_resolution_clock;
+    using TimeUnit = std::chrono::nanoseconds;
+    static inline TimeUnit::rep now()
+    {
+        return std::chrono::duration_cast<TimeUnit>(Clock::now().time_since_epoch()).count();
+    }
+
+private:
+    struct Record
+    {
+        const TimeUnit::rep m_time;
+    };
 };
 
 #endif // LOGGER_H
